b595040fe825f433ec83155ae6ab385536d95739
Vaibhav Gujarathi <gujarathivaibhav789@gmail.com>
2022-09-18 20:39:14 +0530

    modified code to be send

--- New_git_class.java
+++ New_git_class.java
@@ -5,6 +5,7 @@ public class New_git_class {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("My Git program !");
+		System.out.println("Modified code !!");
 	}
 
 }
