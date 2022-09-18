7ed92a9718d2fde6b9a90135f3d2e7c91c610c8b
Vaibhav Gujarathi <gujarathivaibhav789@gmail.com>
2022-09-18 20:35:48 +0530

    New class addedgit add New_git_class.java

---
+++ New_git_class.java
@@ -0,0 +1,10 @@
+package Assignments;
+
+public class New_git_class {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("My Git program !");
+	}
+
+}
