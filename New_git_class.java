50d6cfae33c25bbb8d2cb8f2817c392d391e47ef
Vaibhav Gujarathi <gujarathivaibhav789@gmail.com>
2022-09-18 20:45:41 +0530

    removed code from new class

--- New_git_class.java
+++ New_git_class.java
@@ -5,7 +5,6 @@ public class New_git_class {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("My Git program !");
-		System.out.println("Modified code !!");
 	}
 
 }
